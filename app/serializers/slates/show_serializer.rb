c700fa7d80ac0088dd4c1aa855281df03413ed5a
hyungccho <hyungchulcho93@gmail.com>
2017-03-03 00:08:27 -0800

    expose slate start time strftime formatted

--- app/serializers/slates/show_serializer.rb
+++ app/serializers/slates/show_serializer.rb
@@ -1,9 +1,24 @@
 module Slates
   class ShowSerializer < BaseSerializer
-    attributes :id, :name, :start_time
+    attributes :id, :name, :start_time, :players, :teams
 
-    has_many :players
-    has_many :teams
+    def players
+      PlayersSlate.includes(:player).where(slate_id: object.id).map do |players_slate|
+        ::PlayersSlates::ShowSerializer.new(players_slate).serializable_hash
+      end
+    end
+
+    def teams
+      SlatesTeam.includes(:team).where(slate_id: object.id).map do |players_slate|
+        ::SlatesTeams::ShowSerializer.new(players_slate).serializable_hash
+      end
+    end
+
+    def start_time
+      object.start_time
+            .in_time_zone('America/Los_Angeles')
+            .strftime("%m-%d-%y %I:%M %p %Z")
+    end
 
     def include_players?
       options[:with_players].present?
