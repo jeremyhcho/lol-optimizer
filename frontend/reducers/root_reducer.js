4b014becab2bfe02cbb9eeb18f62487c8a42bed3
hyungccho <hyungchulcho93@gmail.com>
2017-03-03 20:37:22 -0800

    Fix synta errors, move section reducer to admin sub reducer, resolve conflicts

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -2,7 +2,6 @@ import { combineReducers } from 'redux'
 
 // Reducers
 import SessionReducer from 'reducers/session/session_reducer'
-import currentSectionReducer from 'reducers/current_section/current_section_reducer'
 import AdminReducer from 'reducers/admin/admin_reducer'
 
 const RootReducer = combineReducers({
